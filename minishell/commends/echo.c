299fe48b4ea31a8a33639e3a51d8f1eb1f3c2849
Achraf Sabbar <asabbar@e3r6p15.1337.ma>
2022-06-17 15:12:03 +0100

    fixed errors on echo

--- minishell/commends/echo.c
+++ minishell/commends/echo.c
@@ -6,7 +6,7 @@
 /*   By: asabbar <asabbar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:20:16 by asabbar           #+#    #+#             */
-/*   Updated: 2022/06/15 11:51:41 by asabbar          ###   ########.fr       */
+/*   Updated: 2022/06/17 15:10:51 by asabbar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,36 @@ void	ft_putstr(char	*c)
 	}
 }
 
+void	ft_skip_redi(t_list **node, int i)
+{
+	if ((*node)->tokn == WS && ((*node)->next->tokn == IREDI
+			|| (*node)->next->tokn == OREDI || (*node)->next->tokn == OUTPUT_H
+			|| (*node)->next->tokn == INPUT_H))
+		(*node) = (*node)->next;
+	if ((*node)->tokn == IREDI || (*node)->tokn == OREDI
+		|| (*node)->tokn == OUTPUT_H || (*node)->tokn == INPUT_H)
+	{
+		(*node) = (*node)->next;
+		if ((*node)->tokn == WS)
+			(*node) = (*node)->next;
+		if ((*node)->tokn == WR)
+			(*node) = (*node)->next;
+		if ((*node)->tokn == LIMITER)
+			(*node) = (*node)->next;
+		if ((*node)->tokn == WS)
+			(*node) = (*node)->next;
+	}
+	if ((*node)->tokn == WS && !i)
+		(*node) = (*node)->next;
+}
+
 void	ft_echo_flag(t_list *node)
 {
+	if (node->tokn == WS)
+		node = node->next;
 	while (node->tokn != END_TOKN && node->tokn != PIPE)
 	{
-		if (node->tokn == WS && (node->next->tokn == IREDI
-				|| node->next->tokn == OREDI || node->next->tokn == OUTPUT_H
-				|| node->next->tokn == INPUT_H))
-			node = node->next;
-		if (node->tokn == IREDI || node->tokn == OREDI
-			|| node->tokn == OUTPUT_H || node->tokn == INPUT_H)
-		{
-			node = node->next;
-			while (node->tokn == WS)
-				node = node->next;
-			if (node->tokn == WR)
-				node = node->next;
-			if (node->tokn == LIMITER)
-				node = node->next;
-		}
+		ft_skip_redi(&node, 1);
 		if (node->data)
 			ft_putstr(node->data);
 		node = node->next;
@@ -51,28 +62,9 @@ void	ft_echo_flag(t_list *node)
 
 void	ft_echo_utils(t_list *node, int i)
 {
-	if (i == 1)
-	{
-		node = node->next;
-		node = node->next;
-	}
 	while (node && node->tokn != END_TOKN && node->tokn != PIPE)
 	{
-		if (node->tokn == WS && (node->next->tokn == IREDI
-				|| node->next->tokn == OREDI || node->next->tokn == OUTPUT_H
-				|| node->next->tokn == INPUT_H))
-			node = node->next;
-		if (node->tokn == IREDI || node->tokn == OREDI
-			|| node->tokn == OUTPUT_H || node->tokn == INPUT_H)
-		{
-			node = node->next;
-			while (node->tokn == WS)
-				node = node->next;
-			if (node->tokn == WR)
-				node = node->next;
-			if (node->tokn == LIMITER)
-				node = node->next;
-		}
+		ft_skip_redi(&node, 1);
 		if (node->data && node->tokn != END_TOKN)
 			ft_putstr(node->data);
 		node = node->next;
@@ -98,7 +90,7 @@ int	skip_flags(t_list *node)
 
 int	check_flags(t_list *node)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (node->data[i] == '-')
@@ -108,6 +100,21 @@ int	check_flags(t_list *node)
 	if (node->data[i] == 'n' || !node->data[i])
 		return (1);
 	return (0);
+}
+
+void	ft_mini_utils(t_list *head)
+{
+	if ((head->next->tokn == WS || !head->next->tokn)
+		&& (!ft_strcmp(head->data, "-n") || check_flags(head)))
+	{
+		while (skip_flags(head))
+			head = head->next;
+		if (!head->tokn)
+			return ;
+		ft_echo_flag(head);
+	}
+	else
+		ft_echo_utils(head, 0);
 }
 
 void	ft_echo(t_list *node)
@@ -120,26 +127,15 @@ void	ft_echo(t_list *node)
 	{
 		if (!ft_strcmp(head->data, "echo"))
 		{
-			if (head->next->tokn == END_TOKN || head->next->next->tokn == END_TOKN)
+			head = head->next;
+			ft_skip_redi(&head, 0);
+			if (head->tokn == 0)
 				write(1, "\n", 1);
-			else if (head->next->next->data
-				&& !ft_strncmp(head->next->next->data, "-n", 2))
-			{
-				head = head->next->next;
-				if ((head->next->tokn == WS || !head->next->tokn)
-					&& (!ft_strcmp(head->data, "-n") || check_flags(head)))
-				{
-					while (skip_flags(head))
-						head = head->next;
-					if (!head->tokn)
-						return ;
-					ft_echo_flag(head);
-				}
-				else
-					ft_echo_utils(head, 0);
-			}
+			else if (head->data
+				&& !ft_strncmp(head->data, "-n", 2))
+				ft_mini_utils(head);
 			else
-				ft_echo_utils(head, 1);
+				ft_echo_utils(head, 0);
 			ft_lstclear(&node);
 			return ;
 		}
